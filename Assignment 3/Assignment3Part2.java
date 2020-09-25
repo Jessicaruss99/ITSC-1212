ca26802ce9e83c114d247d717166247c9b99ab64
Jessicaruss99 <43121052+Jessicaruss99@users.noreply.github.com>
2020-09-25 16:52:00 -0400

    Add files via upload

---
+++ Assignment 3/Assignment3Part2.java
@@ -0,0 +1,32 @@
+/* Assignment 3, Part 2 - Go Dutch! */
+
+public class Assignment3Part2
+{
+//
+  public static void main(String [] args) throws InterruptedException
+  {
+    String filename;
+    if (args.length > 0) {
+       // got a filename passed into program as a parameter
+       // don't change this part of the code needed by TA for grading
+       filename = args[0];      
+       System.out.println("Filename passed in: " + filename);
+       
+    } else {
+       // ask user for a picture
+       filename = FileChooser.pickAFile();
+       System.out.println("User picked file: " + filename);
+    }
+    Picture pic = new Picture(filename);   // Picture to modify
+    //
+    pic.show();             // Show the original picture
+    Thread.sleep(1000);     //  Pause for 1 second.  You can pause for less if you like
+    
+    Pixel[] pixelArray = pic.getPixels();
+    
+    // TODO: put your flag tinting code here
+    
+
+    pic.repaint();
+  }  // End of main
+}  // End of Class
